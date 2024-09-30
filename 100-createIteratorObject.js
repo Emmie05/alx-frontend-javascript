d2768861f01e7ee6ac2b067a37cded04fbf0f9bf
Emmie05 <emmvicadforster@gmail.com>
2024-09-30 21:40:33 +0000

    Add createIteratorObject function to create an iterator object with all employees

---
+++ 100-createIteratorObject.js
@@ -0,0 +1,10 @@
+// 100-createIteratorObject.js
+
+export default function createIteratorObject(report) {
+    const employees = [];
+    for (const department of Object.values(report.allEmployees)) {
+      employees.push(...department);
+    }
+    return employees;
+  }
+  
