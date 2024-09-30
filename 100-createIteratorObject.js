4056357bd4c02e597343bed82c37b1682843de55
Emmie05 <emmvicadforster@gmail.com>
2024-09-30 22:13:11 +0000

    Refactor block-scoped.js, arrow.js, default-parameter.js, rest-parameter.js, spread-operator.js, and string-interpolation.js

--- 100-createIteratorObject.js
+++
@@ -1,7 +0,0 @@
-export default function createIteratorObject(report) {
-  const employees = [];
-  for (const department of Object.values(report.allEmployees)) {
-    employees.push(...department);
-  }
-  return employees;
-}
