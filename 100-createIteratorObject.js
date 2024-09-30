932416eaf470cb100ba5d78d6dbc83124051ef2b
Emmie05 <emmvicadforster@gmail.com>
2024-09-30 21:43:27 +0000

    Refactor createIteratorObject function to improve readability and maintainability

--- 100-createIteratorObject.js
+++ 100-createIteratorObject.js
@@ -1,10 +1,8 @@
 // 100-createIteratorObject.js
-
 export default function createIteratorObject(report) {
-    const employees = [];
-    for (const department of Object.values(report.allEmployees)) {
-      employees.push(...department);
-    }
-    return employees;
+  const employees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    employees.push(...department);
   }
-  
+  return employees;
+}
